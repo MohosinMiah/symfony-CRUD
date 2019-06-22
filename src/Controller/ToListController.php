10fd0b3d34ba6f370798bcf3294435575f62439b
Mohosin <mohosin16103308@gmail.com>
2019-06-22 08:08:58 +0600

    Add Html Template and created Database

---
+++ src/Controller/ToListController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToListController extends AbstractController
+{
+    /**
+     * @Route("/to/list", name="to_list")
+     */
+    public function index()
+    {
+        return $this->render('to_list/index.html.twig', [
+            'controller_name' => 'ToListController',
+        ]);
+    }
+}
