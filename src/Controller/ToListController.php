f5d8df76b87bc301801ae6df9f9de7d9c7e0c8c2
Mohosin <mohosin16103308@gmail.com>
2019-06-22 12:06:34 +0600

    Create , Switch Status and Read Data

--- src/Controller/ToListController.php
+++ src/Controller/ToListController.php
@@ -5,49 +5,77 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Entity\ToLIst;
 
 class ToListController extends AbstractController
 {
+
+
+ public function taskList()
+ {
+    $entityManager = $this->getDoctrine()->getManager();
+    $task = $this->getDoctrine()
+    ->getRepository(ToLIst::class)
+    ->findAll();
+    return $task;
+ }
+
     /**
      * @Route("/to/list", name="to_list")
      */
     public function index()
     {
+ 
         return $this->render('to_list/index.html.twig', [
-            'controller_name' => 'ToListController',
+            'tasks' =>$this->taskList(),
         ]);
     }
 
     /**
-     * @Route("/create", name="create" methods="POST")
+     * @Route("/create", name="create" )
      */
     public function create(Request $request)
     {
-      // Retrive data from Form
-    //   $title = 
+        $task = new ToLIst();
+        //   Retrive data from Form
+        $title = $request->get('title');
+        if(empty($title)){
+            return $this->redirectToRoute('to_list');
+ 
+        }
+        $task->setTitle($title);
+        $task->setStatus(false);
+        $entityManager = $this->getDoctrine()->getManager();
+        // tell Doctrine you want to (eventually) save the user (no queries yet)
+        $entityManager->persist($task);
+        $entityManager->flush();
 
 
-
-
-
-        return $this->render('to_list/index.html.twig', [
-            'controller_name' => 'ToListController',
-        ]);
+        return $this->redirectToRoute('to_list');
     }
 
     /**
-     * @Route("/switch_status", name="switch_status" methods="POST")
+     * @Route("/switch_status/{id}", name="switch_status" )
      */
-    public function switch_status()
+    public function switch_status($id)
     {
-        return $this->render('to_list/index.html.twig', [
-            'controller_name' => 'ToListController',
-        ]);
+        $entityManager = $this->getDoctrine()->getManager();
+      $task =   $entityManager->getRepository(ToLIst::class)->find($id);
+       //Update status 
+       $task->setStatus(!$task->getStatus());
+
+       $entityManager->flush();
+
+       return $this->redirectToRoute('to_list');
+
     }
 
 
     /**
-     * @Route("/delete", name="delete" methods="POST")
+     * @Route("/delete", name="delete" )
      */
     public function delete()
     {
@@ -55,10 +83,4 @@ class ToListController extends AbstractController
             'controller_name' => 'ToListController',
         ]);
     }
-
-
-   
-
-    //delete
-
 }
